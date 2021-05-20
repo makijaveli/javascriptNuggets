18f89adfd9a82559c442b327f8c0f6313a4bfe5b
Milos Avramovic <avramovic.m@oilpc.ru>
2021-05-20 13:57:36 +0200

    Dynamic Object Keys finish.

---
+++ dynamicObjectKeys/app.js
@@ -0,0 +1,40 @@
+// dot notation
+const person = {
+    name: 'john'
+}
+console.log(person.name);
+person.age = 25;
+console.log(person)
+
+// square bracket notation
+const items = {
+    'featured-items': ['item1', 'item2']
+}
+
+console.log(items['featured-items']);
+console.log(person['name']);
+
+let appState = 'loading';
+const keyName = 'computer';
+// Value is key name
+const app = {
+    [appState]: true
+}
+app[keyName] = 'apple';
+console.log(app)
+
+const state = {
+    loading: true,
+    name: '',
+    job: ''
+}
+
+const updateState = ((key, value) => {
+    state[key] = value
+})
+
+updateState('name', 'john');
+updateState('job', 'developer');
+updateState('loading', 'false');
+updateState('products', []);
+console.log(state)
